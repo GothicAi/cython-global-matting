5edf4ac1e6327bb64008c5e29f99a6f6d3ff2cb5
GothicAi <1751196720@qq.com>
2019-08-27 17:45:10 +0800

    add opencv2

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
                                     sources=["opencv_mat.pyx", "globalmatting.cpp", "guidedfilter.cpp"],
                                     language="c++",
                                     include_dirs=[numpy.get_include(),
-                                                  os.path.join('/usr', 'include', 'opencv2'),
+                                                  'opencv2',
                                                  ],
                                     library_dirs=lib_folders,
                                     libraries=cvlibs,
