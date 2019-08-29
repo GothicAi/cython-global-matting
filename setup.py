017fc11eced0c12b522ef15ecb9babd3a933775b
GothicAi <35886047+GothicAi@users.noreply.github.com>
2019-08-29 19:49:00 +0800

    Revert "Revert "Revert "add opencv2"""
    This reverts commit 89d3d71de61dc3886a4771879fca4b89b47fb046.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ import subprocess
 import sys
 import glob
 
-pthlist = ['opencv2']
+pthlist = []
 
 # search in python(conda) folder
 lib_folder = os.path.join(sys.prefix, 'lib')
@@ -22,12 +22,12 @@ cvlibs = list(set(cvlibs))
 cvlibs = ['-L{}'.format(lib_folder)] + \
          ['opencv_{}'.format(lib.split(os.path.sep)[-1].split('libopencv_')[-1]) for lib in cvlibs]
 
-# conda_path = os.path.join(sys.prefix, 'include', 'opencv2')
-# if os.path.exists(conda_path):
-#     pthlist.append(conda_path)
+conda_path = os.path.join(sys.prefix, 'include', 'opencv2')
+if os.path.exists(conda_path):
+    pthlist.append(conda_path)
 lib_folders = [lib_folder]
 
-if not cvlibs:
+if not pthlist:
     # search in system folder
     libs = os.popen('ldconfig -p | grep libopencv')
     libs = libs.readlines()
@@ -45,29 +45,29 @@ if not cvlibs:
     cvlibs = ['-L{}'.format(lib_folder) for lib_folder in lib_folders] + \
             ['opencv_{}'.format(lib.split(os.path.sep)[-1].split('libopencv_')[-1]) for lib in cvlibs]
 
-    # # Find opencv.hpp in include_folder
-    # proc_incs = subprocess.check_output("pkg-config --cflags opencv".split())
-    # proc_incs = str(proc_incs, 'utf-8').split()
+    # Find opencv.hpp in include_folder
+    proc_incs = subprocess.check_output("pkg-config --cflags opencv".split())
+    proc_incs = str(proc_incs, 'utf-8').split()
 
-    # for i in range(len(proc_incs)):
-    #     proc_incs[i] = proc_incs[i][2:]
-    #     if proc_incs[i].split('/')[-1] == 'opencv':
-    #         proc_incs[i] = proc_incs[i][:-7]
-    # proc_incs = list(set(proc_incs))
-    # for inc in proc_incs:
-    #     inc = inc+'/opencv2'
-    #     if os.path.exists(inc):
-    #         break
+    for i in range(len(proc_incs)):
+        proc_incs[i] = proc_incs[i][2:]
+        if proc_incs[i].split('/')[-1] == 'opencv':
+            proc_incs[i] = proc_incs[i][:-7]
+    proc_incs = list(set(proc_incs))
+    for inc in proc_incs:
+        inc = inc+'/opencv2'
+        if os.path.exists(inc):
+            break
 
-    # default_path = os.path.join('/usr/include/opencv2')
-    # default_local_path = os.path.join('/usr/local/include/opencv2')
+    default_path = os.path.join('/usr/include/opencv2')
+    default_local_path = os.path.join('/usr/local/include/opencv2')
 
-    # if os.path.exists(default_path):
-    #     pthlist.append(default_path)
-    # if os.path.exists(default_path):
-    #     pthlist.append(default_local_path)
-    # if os.path.exists(inc):
-    #     pthlist.append(inc)
+    if os.path.exists(default_path):
+        pthlist.append(default_path)
+    if os.path.exists(default_path):
+        pthlist.append(default_local_path)
+    if os.path.exists(inc):
+        pthlist.append(inc)
 
 
 assert pthlist, 'opencv headers not found, you may refer this doc in [] to solve this error.'
